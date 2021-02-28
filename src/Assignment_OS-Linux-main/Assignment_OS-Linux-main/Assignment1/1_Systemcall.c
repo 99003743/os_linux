e42595a5c54dfab6cdf316fa61c67640c2ca6f0a
99003743 <78868680+99003743@users.noreply.github.com>
2021-03-01 00:15:27 +0530

    Update 1_Systemcall.c

--- src/Assignment_OS-Linux-main/Assignment_OS-Linux-main/Assignment1/1_Systemcall.c
+++ src/Assignment_OS-Linux-main/Assignment_OS-Linux-main/Assignment1/1_Systemcall.c
@@ -2,26 +2,26 @@
 #include<string.h>
 int main(int arg,char* argc[]) 
 { 
-  int fd, sz; 
+  int f, s; 
   char *c = (char *) calloc(100, sizeof(char)); 
   printf("\n%s\n%s",argc[1],argc[2]);
-  fd = open(argc[1], O_RDONLY); 
+  f = open(argc[1], O_RDONLY); 
   if (fd < 0) { 
     perror("r1"); 
   exit(1); 
   }
   
-  sz = read(fd, c, 100); 
-  c[sz] = '\0'; 
+  sz = read(f, c, 100); 
+  c[s] = '\0'; 
   printf("file data: % s\n", c); 
    
-  int fd1 = open(argc[2], O_WRONLY | O_CREAT | O_TRUNC, 0644); 
+  int f1 = open(argc[2], O_WRONLY | O_CREAT | O_TRUNC, 0644); 
   printf("file created\n");
 
-   int k=write(fd1,c,strlen(c));
+   int k=write(f1,c,strlen(c));
 
   // printf("data written\n");
   free(c);
-   close(fd);
+   close(f);
 }
 
