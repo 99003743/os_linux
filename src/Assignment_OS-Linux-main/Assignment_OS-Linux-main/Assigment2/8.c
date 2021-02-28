03a8caa0b1ce90dc4678d6d13cc003f53e75cb6c
99003743 <78868680+99003743@users.noreply.github.com>
2021-03-01 00:22:12 +0530

    Update 8.c

--- src/Assignment_OS-Linux-main/Assignment_OS-Linux-main/Assigment2/8.c
+++ src/Assignment_OS-Linux-main/Assignment_OS-Linux-main/Assigment2/8.c
@@ -7,13 +7,13 @@
  
 int main() 
 { 
- const int SIZE = 4096; 
- const char* name = "OS"; 
+ const int S = 4096; 
+ const char* n = "OS"; 
  int shm_fd; 
  void* ptr; 
  shm_fd = shm_open(name, O_RDONLY, 0666); 
- ptr = mmap(0, SIZE, PROT_READ, MAP_SHARED, shm_fd, 0); 
+ ptr = mmap(0, S, PROT_READ, MAP_SHARED, shm_fd, 0); 
  printf("%s", (char*)ptr); 
- shm_unlink(name); 
+ shm_unlink(n); 
  return 0; 
 }
