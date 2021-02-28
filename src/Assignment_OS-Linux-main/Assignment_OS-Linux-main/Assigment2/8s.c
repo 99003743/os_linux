77ccf4195b64369dabc0ccf30acf370a1a199472
99003743 <78868680+99003743@users.noreply.github.com>
2021-03-01 00:23:35 +0530

    Update 8s.c

--- src/Assignment_OS-Linux-main/Assignment_OS-Linux-main/Assigment2/8s.c
+++ src/Assignment_OS-Linux-main/Assignment_OS-Linux-main/Assigment2/8s.c
@@ -8,18 +8,18 @@
  
 int main() 
 { 
- const int SIZE = 4096; 
- const char* name = "OS"; 
- const char* message_0 = "Hello"; 
- const char* message_1 = "From Ankit"; 
+ const int S = 4096; 
+ const char* n = "OS"; 
+ const char* msg_0 = "Hello"; 
+ const char* msg_1 = "From Ankit"; 
  int shm_fd; 
  void* ptr; 
  shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666); 
- ftruncate(shm_fd, SIZE); 
- ptr = mmap(0, SIZE, PROT_WRITE, MAP_SHARED, shm_fd, 0); 
- sprintf(ptr, "%s", message_0); 
- ptr += strlen(message_0); 
- sprintf(ptr, "%s", message_1); 
- ptr += strlen(message_1); 
+ ftruncate(shm_fd, S); 
+ ptr = mmap(0, S, PROT_WRITE, MAP_SHARED, shm_fd, 0); 
+ sprintf(ptr, "%s", msg_0); 
+ ptr += strlen(msg_0); 
+ sprintf(ptr, "%s", msg_1); 
+ ptr += strlen(msg_1); 
  return 0; 
 }
